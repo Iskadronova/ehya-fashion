ebc7a10c2e1709b5ec7092e706942b3d0867f503
Iskadronova <Candy__88@mail.ru>
2020-08-13 20:06:46 +0500

    fix: main.js tabs visible

--- js/main.js
+++ js/main.js
@@ -17,4 +17,17 @@ $(document).ready(function () {
     if (event.keyCode == 27) $(".menu").toggleClass("menu_visible");
     $(".close").toggleClass("close_visible");
   });
+
+  //Табы
+  var tabsItem = $(".trending-category__link");
+  var contentItem = $(".trending-card");
+
+  tabsItem.on("click", function (event) {
+    var activeContent = $(this).attr("data-target");
+    tabsItem.removeClass("trending-category__link--active");
+    contentItem.removeClass("trending-card--visible");
+    $(activeContent).addClass("trending-card--visible");
+    $(this).addClass("trending-category__link--active");
+    // console.log(activeContent);
+  });
 });
